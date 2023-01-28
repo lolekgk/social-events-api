f68c406f07b92b6f890b19285246d56d2cf58b38
Karol Gajda <karol.gajda97@gmail.com>
2023-01-28 23:14:35 +0100

    Add User and UserGroup models

--- users/models.py
+++ users/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    friends = models.ManyToManyField('self')
+    profile_picture = models.ImageField(
+        upload_to='user-profile-images/', blank=True
+    )
+
+
+class UserGroup(models.Model):
+    name = models.TextField(max_length=255)
+    administrators = models.ManyToManyField(User, related_name='groups_admin')
+    members = models.ManyToManyField(User, related_name='groups_member')
+
+    def __str__(self) -> str:
+        return self.name
