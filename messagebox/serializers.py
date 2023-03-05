c50598c3095ba02ea891ddc690089162e97c7436
Karol Gajda <karol.gajda97@gmail.com>
2023-03-05 22:10:08 +0100

    Implement serializers for messagebox models

---
+++ messagebox/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from .models import Message, MessageThread
+
+User = get_user_model()
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.PrimaryKeyRelatedField(read_only=True)
+    receiver = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    thread = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = [
+            "id",
+            "sender",
+            "receiver",
+            "content",
+            "date_sent",
+            "read_status",
+            "thread",
+        ]
+
+    def validate(self, attrs: dict) -> dict:
+        instance = Message(**attrs)
+        instance.clean()
+        return attrs
+
+
+class MessageThreadSerializer(serializers.ModelSerializer):
+    participants = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), many=True
+    )
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = MessageThread
+        fields = ["id", "participants", "messages", "created_at"]
