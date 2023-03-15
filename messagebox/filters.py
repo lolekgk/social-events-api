a797a031d53e85f1e88394e4a1b9977de053379e
Karol Gajda <karol.gajda97@gmail.com>
2023-03-15 22:13:20 +0100

    Refactor filter method

--- messagebox/filters.py
+++ messagebox/filters.py
@@ -13,14 +13,10 @@ class MessageFilter(FilterSet):
 
     def filter_message_direction(self, queryset, name, value):
         user = self.request.user
-        filter_conditions = {
-            "sent": queryset.filter(sender=user, deleted_by_sender=False),
-            "received": queryset.filter(
-                receiver=user, deleted_by_receiver=False
-            ),
-        }
-        if value in filter_conditions:
-            return filter_conditions[value]
+        if value == "sent":
+            return queryset.filter(sender=user, deleted_by_sender=False)
+        elif value == "received":
+            return queryset.filter(receiver=user, deleted_by_receiver=False)
         return queryset.filter(
             Q(sender=user, deleted_by_sender=False)
             | Q(receiver=user, deleted_by_receiver=False)
