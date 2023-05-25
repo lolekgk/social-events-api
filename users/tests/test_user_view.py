09a67d85b2e60c27bf540d8d340bd10446e0de1f
Karol Gajda <karol.gajda97@gmail.com>
2023-05-25 22:52:34 +0200

    Add tests for updating users

--- users/tests/test_user_view.py
+++ users/tests/test_user_view.py
@@ -6,6 +6,28 @@ from rest_framework.test import APIClient
 from users.models import User
 
 
+@pytest.fixture
+def user_update_payload_short():
+    payload = {
+        "first_name": "test",
+        "last_name": "test",
+    }
+    yield payload
+    del payload
+
+
+@pytest.fixture
+def user_update_payload():
+    payload = {
+        "first_name": "test",
+        "last_name": "test",
+        "birth_date": "1990-01-01",
+        "friends": [2, 3],
+    }
+    yield payload
+    del payload
+
+
 @pytest.mark.django_db
 class TestListUser:
     def test_get_users(self, api_client: APIClient) -> None:
@@ -41,6 +63,85 @@ class TestRetrieveUser:
         response = api_client.get("/users/1/", format="json")
 
         assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+class TestUpdateUser:
+    def test_update_user_as_anonymous_user(
+        self, api_client: APIClient, user_update_payload_short
+    ) -> None:
+        user = baker.make(User, pk=1)
+
+        response = api_client.patch(
+            f"/users/{user.pk}/", data=user_update_payload_short, format="json"
+        )
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_update_other_user(
+        self, api_client: APIClient, user_update_payload_short
+    ) -> None:
+        current_user = baker.make(User, pk=2)
+        api_client.force_authenticate(current_user)
+        user_to_update = baker.make(User, pk=1)
+
+        response = api_client.patch(
+            f"/users/{user_to_update.pk}/",
+            data=user_update_payload_short,
+            format="json",
+        )
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_update_user_own_profile_with_non_existing_friends(
+        self, api_client: APIClient, user_update_payload
+    ) -> None:
+        current_user = baker.make(User, pk=1)
+        api_client.force_authenticate(current_user)
+
+        response = api_client.put(
+            f"/users/{current_user.pk}/",
+            data=user_update_payload,
+            format="json",
+        )
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_update_user_own_profile_short(
+        self, api_client: APIClient, user_update_payload_short
+    ) -> None:
+        user = baker.make(User, pk=1)
+        api_client.force_authenticate(user)
+
+        response = api_client.patch(
+            f"/users/{user.pk}/", data=user_update_payload_short, format="json"
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["first_name"] == "test"
+        assert response.data["last_name"] == "test"
+        assert response.data["email"] == user.email
+        assert response.data["username"] == user.username
+
+    def test_update_user_own_profile(
+        self, api_client: APIClient, user_update_payload
+    ) -> None:
+        user = baker.make(User, pk=1)
+        api_client.force_authenticate(user)
+        friend_one = baker.make(User, pk=2)
+        friend_two = baker.make(User, pk=3)
+
+        response = api_client.put(
+            f"/users/{user.pk}/", data=user_update_payload, format="json"
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["first_name"] == "test"
+        assert response.data["last_name"] == "test"
+        assert response.data["email"] == user.email
+        assert response.data["username"] == user.username
+        assert response.data["birth_date"] == "1990-01-01"
+        assert friend_one.pk in response.data["friends"]
 
 
 @pytest.mark.django_db
